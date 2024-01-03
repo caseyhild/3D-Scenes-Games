d8ed214d80d575ece48cbc5c0607dbe03041a353
Casey <44584719+caseyhild@users.noreply.github.com>
2024-01-03 05:59:51 -0800

    Delete Zombie Apocalypse directory

--- Zombie Apocalypse/ClearScores.java
+++
@@ -1,9 +0,0 @@
-import java.io.*;
-public class ClearScores
-{
-    public static void main(String[] args) throws IOException
-    {
-        //uncomment this to be able to clear all scores
-        //PrintWriter scores = new PrintWriter(new File("scores.txt"));
-    }
-}
