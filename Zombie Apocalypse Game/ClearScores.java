f7d7b5a7ce5f57f8db6407b80844b8043604b3a1
Casey <44584719+caseyhild@users.noreply.github.com>
2024-01-03 06:00:26 -0800

    Add files via upload

---
+++ Zombie Apocalypse Game/ClearScores.java
@@ -0,0 +1,9 @@
+import java.io.*;
+public class ClearScores
+{
+    public static void main(String[] args) throws IOException
+    {
+        //uncomment this to be able to clear all scores
+        //PrintWriter scores = new PrintWriter(new File("scores.txt"));
+    }
+}
